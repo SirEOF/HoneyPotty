15e76a4a24a61239d579396d3e16a3df9c5616e2
Tiago Peralta <tperalta82@gmail.com>
2017-03-07 17:17:10 +0000

    Added better instructions message

--- cli/geniptables.php
+++ cli/geniptables.php
@@ -2,7 +2,7 @@
 $path = dirname(__DIR__);
 require_once "$path/common/common.php";
 
-echo "Please run: iptables -N honeypot; iptables -I INPUT -j honeypot; before running this or it will NOT work".PHP_EOL;
+echo "Please run: iptables -N honeypot;iptables -I INPUT -j honeyput -p tcp -m multiport --dport 80; before running this or it will NOT work".PHP_EOL;
 echo "Although it might not be secure, run this script as root in order to add the rules to your firewall, set it as a */1 cron for example".PHP_EOL;
 $apply = false;
 if (isset($argv[1]) && $argv[1] === "apply") {
